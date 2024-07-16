278a75c05b7d7655aaf21b5b1493f537a0730daa
Sirani McNeill <siranimcneill020@gmail.com>
2024-07-16 14:43:07 +1000

    index.js

---
+++ Javascript/index.js
@@ -0,0 +1,31 @@
+<script>
+    
+function tritColour() {
+  var element = document.body;
+  element.classList.toggle("tritbody");
+  var element = document.index;
+  element.classList.toggle("tritbutton");
+}
+
+function deutColour() {
+  var element = document.body;
+  element.classList.toggle("deutbody");
+  var element = document.index;
+  element.classList.toggle("deutbutton");
+}
+
+function tritColour() {
+  var element = document.body;
+  element.classList.toggle("monobody");
+  var element = document.index;
+  element.classList.toggle("monobutton");
+}
+
+function typColour() {
+  var element = document.body;
+  element.classList.toggle("typbody");
+  var element = document.index;
+  element.classList.toggle("typbutton");
+}
+    
+</script>
