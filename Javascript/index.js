83018f37a1ff866fce7dd792c845c86ab507bd82
Myoty <160827496+Myoty@users.noreply.github.com>
2024-07-16 15:40:59 +1000

    Update index.js
    Signed-off-by: Myoty <160827496+Myoty@users.noreply.github.com>

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,20 +1,20 @@
 function tritColour() {
   var element = document.body;
   element.classList.toggle("tritBody");
-  var element = document.index;
+  var element = document.mainbutton;
   element.classList.toggle("tritButton");
 }
 
 function deutColour() {
   var element = document.body;
   element.classList.toggle("deutBody");
-  var element = document.index;
+  var element = document.mainbutton;
   element.classList.toggle("deutButton");
 }
 
 function monoColour() {
   var element = document.body;
   element.classList.toggle("monoBody");
-  var element = document.index;
+  var element = document.mainbutton;
   element.classList.toggle("monoButton");
 }
